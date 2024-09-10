2317286782eaf2cbbbf421fcd8741692744001f7
seraeac <rodolphosilva.ale@gmail.com>
2024-09-10 09:49:51 -0300

    Criando o metodo Editar cliente

--- Sistema de Vendas/src/dao/ClientesDAO.java
+++ Sistema de Vendas/src/dao/ClientesDAO.java
@@ -61,6 +61,47 @@ public class ClientesDAO {
         }
         
     }
+    public void Editar(Clientes obj){
+        
+        try {
+            //criando o SQL
+            
+
+            String sql = "update tb_clientes set nome=?,rg=?,cpf=?,email=?,"
+                    + "telefone=?,celular=?,cep=?,endereco=?,numero=?,"
+                    + "complemento=?,bairro=?,cidade=?,estado=? where id=?";
+                    
+            
+            //preparar conexao sql para se conectar ao banco
+            
+            PreparedStatement stmt=conn.prepareStatement(sql);
+            
+            stmt.setString(1,obj.getNome());
+            stmt.setString(2,obj.getRg());
+            stmt.setString(3,obj.getCpf());
+            stmt.setString(4,obj.getEmail());
+            stmt.setString(5,obj.getTelefone());
+            stmt.setString(6,obj.getCelular());
+            stmt.setString(7,obj.getCep());
+            stmt.setString(8,obj.getEndereco());
+            stmt.setInt(9,obj.getNumero());
+            stmt.setString(10,obj.getComplemento());
+            stmt.setString(12,obj.getBairro());
+            stmt.setString(11,obj.getCidade());
+            stmt.setString(13,obj.getEstado());
+            stmt.setInt(14,obj.getId());
+            
+            // executa SQL
+            stmt.execute();
+            //fecha conecxao
+            stmt.close();
+            JOptionPane.showMessageDialog(null,"Cliente Editado com sucesso");
+        } catch (SQLException erro) {
+            
+            JOptionPane.showMessageDialog(null,"Erro ao Editar o Cliente "+erro.getMessage());
+        }
+        
+    }
     public Clientes buscarCliente(String nome){
        
         try {
