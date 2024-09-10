6ce5e3e57abec765d23a906eab8f30ab22e10fa5
seraeac <rodolphosilva.ale@gmail.com>
2024-09-10 08:12:41 -0300

    criação do metodo filtrar por nome

--- Sistema de Vendas/src/dao/ClientesDAO.java
+++ Sistema de Vendas/src/dao/ClientesDAO.java
@@ -127,4 +127,40 @@ public class ClientesDAO {
         return null;
     }
     
+    public List<Clientes>filtrar(String nome){
+       List<Clientes> lista = new ArrayList<>();
+       
+        try {
+            String sql= "select * from tb_clientes where nome like ?";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1,nome);
+            ResultSet rs= stmt.executeQuery();
+            
+            while(rs.next()){
+                
+                Clientes obj = new Clientes();
+                obj.setId(rs.getInt("id"));
+                obj.setNome(rs.getString("nome"));
+                obj.setRg(rs.getString("rg"));
+                obj.setCpf(rs.getString("cpf"));
+                obj.setEmail(rs.getString("email"));
+                obj.setTelefone(rs.getString("telefone"));
+                obj.setCelular(rs.getString("celular"));
+                obj.setCep(rs.getString("cep"));
+                obj.setEndereco(rs.getString("endereco"));
+                obj.setNumero(rs.getInt("numero"));
+                obj.setComplemento(rs.getString("complemento"));
+                obj.setBairro(rs.getString("bairro"));
+                obj.setCidade(rs.getString("cidade"));
+                obj.setEstado(rs.getString("estado"));
+                
+                lista.add(obj);
+            }
+            return lista;
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null,"Erro ao criar lista: " +erro.getMessage());
+        }
+        return null;
+    }
+    
 }
